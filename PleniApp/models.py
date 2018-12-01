89f5e9784cc1bf7defeac40089c68a9aa706bbb3
Alex Gu Du <alex.gmole@gmail.com>
2018-11-30 16:36:25 -0800

    model change

--- PleniApp/models.py
+++ PleniApp/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 # Create your models here.
-class Thread(models.Model):
+class Lecture(models.Model):
     title = models.CharField(max_length=50)
+    description = models.TextField()
     videourl = models.CharField(default='empty',max_length=200)
+    unit_index = models.IntegerField()
+    lecture_type_index = models.IntegerField()
