0908bcda84e95fab812147b932234f083cd98774
Alex Gu Du <alex.gmole@gmail.com>
2018-11-30 16:39:26 -0800

    revert model change in file

--- PleniApp/models.py
+++ PleniApp/models.py
@@ -1,9 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class Lecture(models.Model):
+class Thread(models.Model):
     title = models.CharField(max_length=50)
-    description = models.TextField()
     videourl = models.CharField(default='empty',max_length=200)
-    unit_index = models.IntegerField()
-    lecture_type_index = models.IntegerField()
