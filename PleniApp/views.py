50fef2a47f51189182328642353c02c75769efc8
Alex Gu Du <alex.gmole@gmail.com>
2018-12-03 23:31:42 -0800

    collapsable text in jumbotron, fontawesome impl

--- PleniApp/views.py
+++ PleniApp/views.py
@@ -18,11 +18,18 @@ def index_selected_unit(request, id):
     for selected_lecture in selected_lectures:
         selected_lecture.navlinkid += str(selected_lecture.lecture_index_number)
         navlinkid_list.append(selected_lecture.navlinkid)
+    if id==1:
+        unit_description = "En la primera unidad se nos presentan conceptos básicos de la computadora, dispositivos de entrada y salida que nos ayudan para poder manejar la computadora a introducir la información, también vamos a empezar a manejar ciertos programas que nos ayudaran a familiarizar con la computadora y el usuario, como lo es Paint, calculadora y Word, también tendremos una introducción sobre internet, como realizar búsquedas, usar correo y redes sociales para que se puedan mantener conectados."
+    if id==2:
+        unit_description = "En la segunda unidad se nos presentan conceptos ya avanzados de Word donde podremos usar más opciones que nos permitan hacer mayor énfasis en nuestros textos dando un mejor resultado en ellos agregando títulos, imágenes, figuras, cuadros y más textos coloridos. Posterior a esto empezamos con conceptos básicos de Excel, como son sumas, restas, multiplicaciones y divisiones, también a poder darle formato a secuencias de números para llevar un mejor control sobre nuestra información, y para finalizar veremos un poco de fórmulas en Excel para tener mayor contacto con la información que ingresemos."
+    if id==3:
+        unit_description = "En la tercera unidad veremos conceptos de PowerPoint y Publisher, donde se podrán aplicar conceptos de la unidad 1 y 2 que nos apoyarán en realizar presentaciones básicas y bien hechas para que los adultos las presenten durante la clase. "
     unit = str(id)
     context = {
         'lectures': selected_lectures,
         'navlinkid_list': navlinkid_list,
         'unit': unit,
+        'unit_description': unit_description
     }
     return render(request, 'pleniapp/index_selected_unit.html', context)
 
