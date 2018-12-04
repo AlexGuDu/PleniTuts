1dc1a5d86942fec2fba27ceca02ed20a06f5b510
Alex Gu Du <alex.gmole@gmail.com>
2018-12-03 21:40:55 -0800

    aesthetic and new template for lecturetypes

--- PleniApp/urls.py
+++ PleniApp/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('index/', views.index, name='index'),
     path('details/<int:id>/', views.details, name='details'),
     path('lectureblock/<int:id>/', views.index_selected_unit, name='index_selected_unit'),
+    path('lecturetype/<int:id>/', views.index_selected_type, name='index_selected_type'),
 ]
--- PleniApp/views.py
+++ PleniApp/views.py
@@ -26,6 +26,19 @@ def index_selected_unit(request, id):
     }
     return render(request, 'pleniapp/index_selected_unit.html', context)
 
+def index_selected_type(request, id):
+    selected_lectures = Lecture.objects.filter(lecture_type_index=id)
+    navlinkid_list = list()
+    for selected_lecture in selected_lectures:
+        selected_lecture.navlinkid += str(selected_lecture.lecture_index_number)
+        navlinkid_list.append(selected_lecture.navlinkid)
+    unit = str(id)
+    context = {
+        'lectures': selected_lectures,
+        'navlinkid_list': navlinkid_list
+    }
+    return render(request, 'pleniapp/index_selected_type.html', context)
+
 
 def details(request, id):
     lecture = Lecture.objects.get(id=id)
